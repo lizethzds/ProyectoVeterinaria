9e24bd82fe55d6b10099f4a2069e06ff422c5fc7
Lizeth Zurutuza <111436787+lizethzds@users.noreply.github.com>
2023-06-10 14:52:12 -0600

    Citas entidad

---
+++ Veterinaria/src/main/java/com/example/veterinaria/controladores/VeterinarioController.java
@@ -0,0 +1,4 @@
+package com.example.veterinaria.controladores;
+
+public class VeterinarioController {
+}
--- Veterinaria/src/main/java/com/example/veterinaria/modelos/Citas.java
+++ Veterinaria/src/main/java/com/example/veterinaria/modelos/Citas.java
@@ -1,4 +1,48 @@
 package com.example.veterinaria.modelos;
 
 public class Citas {
+
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String especialidad;
+
+    public Citas(int id, String nombre, String telefono, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
 }
